fd6340782f0146e2488aa545d6ae657251e50528
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-11-05 09:37:10 +0000

    ..

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def main():
 
     # Initialize transformer to None
     transformer = None
-
+    st.write(json_uploader.get_dataframe())
     # Check if both files are uploaded and create the transformer object
     if df_Parquet is not None and json_rule is not None:
         transformer = DataTransformer(df_Parquet, json_uploader.get_dataframe())
