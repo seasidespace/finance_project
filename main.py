6d98a9e19c654adf4b997cf80edb8c42b18397df
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-11-05 07:43:25 +0000

    ...

--- main.py
+++ main.py
@@ -32,45 +32,44 @@ def main():
     display_text_as_large("1. Upload your dataset for processing")
 
     myFile = FileUploader()
-
-    # Use the upload_file method to let the user upload a file
     label = "Upload a Parquet file"
     uploaded_file = myFile.upload_file(label, "parquet")
-    
+    df_Parquet = None  # Initialize to None
 
     # If a file was uploaded, read the Parquet file
     if uploaded_file:
         myFile.read_parquet(uploaded_file)
-
-        # Get the DataFrame and display it
         df_Parquet = myFile.get_dataframe()
         if df_Parquet is not None:
             st.dataframe(df_Parquet.head())  # Display the first few rows of the DataFrame
 
- 
     # title for prompting json file 
     display_text_as_large("2. Upload the transformation you want to apply")
     
-    # Upload the transformation you want to apply
     json_uploader = JSONFileUploader()
     json_uploader.upload_json_file()
+    json_rule = json_uploader.get_json_data()  # Initialize to None
 
-    # Retrieve the JSON data
-    json_rule = json_uploader.get_json_data()
     if json_rule is not None:
         json_uploader.display_dataframe()
 
-    # Create an instance of the DataTransformer with the global data
-    if df_Parquet and json_rule:
+    # Initialize transformer to None
+    transformer = None
+
+    # Check if both files are uploaded and create the transformer object
+    if df_Parquet is not None and json_rule is not None:
         transformer = DataTransformer(df_Parquet, json_rule)
 
     # Button to apply transformations
     if st.button("Apply Transformations"):
-        transformer.apply_transformations()
-        transformed_df = transformer.get_transformed_dataframe()
-        if transformed_df is not None:
-            st.dataframe(transformed_df)
-
-    
+        if transformer:  # Check if transformer is not None
+            transformer.apply_transformations()
+            transformed_df = transformer.get_transformed_dataframe()
+            if transformed_df is not None:
+                st.dataframe(transformed_df)
+        else:
+            st.error("Please upload both Parquet and JSON files before applying transformations.")
 
 main()
+
+    
