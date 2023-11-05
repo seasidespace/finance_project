a47b2f0a07416bc90f3340c723c862aa3be47ccc
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-11-05 03:31:52 +0000

    set max file size to 200gb

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# Importing the Snowflake Connector for Python
 import snowflake.connector
-
-# Importing Snowflake's Snowpark library
 from snowflake.snowpark import Session
-
-# Importing Snowflake SQLAlchemy
 from snowflake.sqlalchemy import URL
-
-# Importing Streamlit
 import streamlit as st
-
-# Importing OpenAI
 import openai
+
