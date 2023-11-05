7aafdce1c82eccee9c70c1c636a7662dc6fdee46
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-11-05 03:23:49 +0000

    config requirements

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# Importing the Snowflake Connector for Python
+import snowflake.connector
 
+# Importing Snowflake's Snowpark library
+from snowflake.snowpark import Session
+
+# Importing Snowflake SQLAlchemy
+from snowflake.sqlalchemy import URL
+
+# Importing Streamlit
+import streamlit as st
+
+# Importing OpenAI
+import openai
