64398880cf9d95f72fa08d41f4e554e15d79eb72
seasidespace <144977810+seasidespace@users.noreply.github.com>
2023-11-05 05:33:48 +0000

    add text

--- main.py
+++ main.py
@@ -6,15 +6,37 @@ import openai
 from file_uploader import FileUploader
 
 
+def display_text_as_large(text):
+    # Define the HTML structure with style
+    large_text_html = f"""
+        <style>
+        .big-font {{
+            font-size:30px !important;
+        }}
+        </style>
+        <div class='big-font'>
+            {text}
+        </div>
+        """
+    st.markdown(large_text_html, unsafe_allow_html=True)
+
+
 def main():
     st.title("Data Ingestion Tool")
     
+    '''
+    Upload your dataset for processing
+    '''
+
+    display_text_as_large("1. Upload your dataset for processing")
+
     myFile = FileUploader()
 
     # Use the upload_file method to let the user upload a file
     label = "Only files in Parquet format are permitted."
     uploaded_file = myFile.upload_file(label, "parquet")
     
+
     # If a file was uploaded, read the Parquet file
     if uploaded_file:
         myFile.read_parquet(uploaded_file)
@@ -24,5 +46,12 @@ def main():
         if df is not None:
             st.write("Quick View of the Uploaded File:")
             st.dataframe(df.head())  # Display the first few rows of the DataFrame
+
+    '''
+    Upload the transformation you want to apply
+    '''
+    
+    display_text_as_large("2. Upload the transformation you want to apply")
+
 
 main()
